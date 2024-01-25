e8a37b6416e5870241e45761acff11a085f0fb53
Artem <akartemka2@gmail.com>
2024-01-25 08:44:56 +0200

    official announcement URL

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
             "accept": "text/x-component",
             "accept-language": "en-US,en;q=0.9,ru;q=0.8",
             "content-type": "text/plain;charset=UTF-8",
-            "next-action": "67fc7bba70928774c5305f604afe585929202b77",
+            "next-action": "6817e8f24aae7e8aed1d5226e9b368ab8c1ded5d",
             "next-router-state-tree": "%5B%22%22%2C%7B%22children%22%3A%5B%22(homePage)%22%2C%7B%22children%22%3A%5B%22__PAGE__%22%2C%7B%7D%5D%7D%5D%7D%2Cnull%2Cnull%2Ctrue%5D",
             "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
             "sec-ch-ua-mobile": "?0",
@@ -25,12 +25,13 @@ def main():
             "sec-fetch-mode": "cors",
             "sec-fetch-site": "same-origin",
             'user-agent': ua.random,
-            "Referer": 'https://94c87e96-000a-4740-bc01-0ca9abbb7bca-stg-airdrop.alt.technology/'
+            "Referer": 'https://airdrop.altlayer.io/',
+            "Origin": 'https://airdrop.altlayer.io/'
         }
 
         while True:
             try:
-                response = requests.post('https://94c87e96-000a-4740-bc01-0ca9abbb7bca-stg-airdrop.alt.technology/', data=json.dumps([address]), headers=headers)
+                response = requests.post('https://airdrop.altlayer.io/', data=json.dumps([address]), headers=headers)
                 if response.status_code != 200:
                     raise Exception(f'received non-200 code - {response.status_code}')
                 amount_match = re.search(r'"amount":"([^"]+)"', response.text)
